911f6f5dd22d190100f696eb10dd080e7d23cb89
nikitsch <nikitsch5@gmail.com>
2021-09-06 21:09:52 +0300

    init: add code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.average
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        this.average = Math.ceil((this.max - this.min) / 2 + this.min)
+        return this.average
     }
 
     lower() {
-
+        this.max = this.average
+        return this.max
     }
 
     greater() {
-
+        this.min = this.average
+        return this.min
     }
 }
 
